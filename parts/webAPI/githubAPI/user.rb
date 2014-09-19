bb42a0cfd1ae47783020a75d6b44adb968670419
amazedkoumei <amazed.koumei@gmail.com>
2014-09-20 08:42:30 +0900

    add method star, unstar, watch, unwatch, follow, unfollow to github api

--- parts/webAPI/githubAPI/user.rb
+++ parts/webAPI/githubAPI/user.rb
@@ -9,9 +9,48 @@ module AMP
       end
     end
 
+    def starRepository(owner, repo, &block)
+      request(API_ROOT + "/user/starred/#{owner}/#{repo}", :put) do |response, query|
+        block.call(response.status_code == 204) unless block.nil?
+      end
+    end
+
+    def unstarRepository(owner, repo, &block)
+      request(API_ROOT + "/user/starred/#{owner}/#{repo}", :delete) do |response, query|
+        block.call(response.status_code == 204) unless block.nil?
+      end
+    end
+
+    def watchRepository(owner, repo, &block)
+      request(API_ROOT + "/user/subscriptions/#{owner}/#{repo}", :put) do |response, query|
+        block.call(response.status_code == 204) unless block.nil?
+      end
+    end
+
+    def unwatchRepository(owner, repo, &block)
+      request(API_ROOT + "/user/subscriptions/#{owner}/#{repo}", :delete) do |response, query|
+        block.call(response.status_code == 204) unless block.nil?
+      end
+    end
+
     def isFollowingUser(user, &block)
       request(API_ROOT + "/user/following/#{user}") do |response, query|
-        # http://developer.github.com/v3/users/followers/#check-if-you-are-following-a-user
+        # http://developer.github.com/v3/users/followers/#check-if-you-are-following-a-user        
+        p response.status_code
+        block.call(response.status_code == 204) unless block.nil?
+      end
+    end
+
+    def followUser(user, &block)
+      p user
+      request(API_ROOT + "/user/following/#{user}", :put) do |response, query|
+        p response.status_code
+        block.call(response.status_code == 204) unless block.nil?
+      end
+    end
+
+    def unfollowUser(user, &block)
+      request(API_ROOT + "/user/following/#{user}", :delete) do |response, query|
         block.call(response.status_code == 204) unless block.nil?
       end
     end
